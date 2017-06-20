21315a71781b01c5f21c60f0cd26023c5e092558
uthistra777 <muthumarisubha97@gmail.com>
2017-06-20 16:48:22 +0530

    Create vowcon.java

---
+++ vowcon.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class vowcon
+{
+        public static void main(String[] args)
+        {
+                char ch = 'i';
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+        System.out.println(ch + " is vowel");
+    else
+      System.out.println(ch + " is consonant");
+
+  }
+}
